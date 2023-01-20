ef62d5d1947515bdcefd2c1fc07e618d453a79cd
CrackNicolas <beltrannicolasalejo@gmail.com>
2023-01-20 19:47:28 -0300

    Actualizacion v2.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path : '', component : HomeComponent},
-  {path : 'inicio', component : InicioComponent}
+  {path : '', component : InicioComponent},
+  {path : 'portafolio', component : HomeComponent}
 ];
 
 @NgModule({
